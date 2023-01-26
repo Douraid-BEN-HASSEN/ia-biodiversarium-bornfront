ae75c16ba05200b4461121336a211f9dab8dc9ef
Alexis DEVLEESCHAUWER <alexis.devleeschauwer@imerir.com>
2023-01-26 15:46:24 +0100

    replace old flux by hls

--- biodiversarium-bornfront/src/config.tsx
+++ biodiversarium-bornfront/src/config.tsx
@@ -1,10 +1,10 @@
 const API_URL: string = process.env.REACT_APP_API_URL ?? "http://10.3.1.37"
 
 export enum camera {
-    CAMERA_OUT='http://localhost:8000/out',
-    CAMERA_IN='http://localhost:8000/in',
+    CAMERA_OUT='video/1/stream.m3u8',
+    CAMERA_IN='video/test/stream.m3u8',
+    CAMERA_TEST='video/test/stream.m3u8',
 }
-
 export const config = {
     IA_API_TABLET_ANALYZE: `${API_URL}/api/tablet/analyze`
 }
